6f358baa37e1fc2db4ccb685d8e43c64c36a145e
Amandeep Midha <amandeepm@gmx.com>
2016-04-30 01:52:43 +0200

    revised

--- nordea-parser/index.js
+++ nordea-parser/index.js
@@ -8,59 +8,59 @@ var dateFmt = require("date-format-lite");
 var random = require("random-js")(); // using the nativeMath engine
 
 
-var dbconfig = require(__dirname+'\\config.js');
-var txn_rules = require(__dirname+'\\txn-classification-rules.js');
+var dbconfig = require('./config.js');
+var txn_rules = require('./txn-classification-rules.js');
 var R = new RuleEngine(txn_rules.rules);
-var rowCount =0, txnLines = 0;
-var dbCon ;
+var rowCount = 0, txnLines = 0;
+var dbCon;
 
 var args = process.argv.slice(2);
-if(args.length == 0) {
+if (args.length === 0) {
     console.log("Please provide Bank Statement File name (full path) to parse as argument ... Exiting! ");
     process.exit(0);
 }
 
-String.prototype.replaceAll = function(search, replacement) {
+String.prototype.replaceAll = function (search, replacement) {
     var target = this;
     return target.replace(new RegExp(search, 'g'), replacement);
 };
 
 var tempFile = random.integer(1, 1000) + '.dat';
-var parser = parse({delimiter: ';', skip_empty_lines:true, columns: ['Date','Text','TxDate','Amount','Balance'], auto_parse: true});
+var parser = parse({delimiter: ';', skip_empty_lines: true, columns: ['Date', 'Text', 'TxDate', 'Amount', 'Balance'], auto_parse: true});
 
-fs.readFileSync('./'+args[0]).toString().split('\n').forEach(function (line) {
-  if(line.trim().length > 0 && rowCount > 0) { //Only if it is non-empty line & Skip first non-empty line i.e. header as well
-      fs.appendFileSync("./"+tempFile, line.toString().replaceAll(",",".") + "\n");
-  }
-  rowCount++;
+fs.readFileSync('./' + args[0]).toString().split('\n').forEach(function (line) {
+    if (line.trim().length > 0 && rowCount > 0) { //Only if it is non-empty line & Skip first non-empty line i.e. header as well
+        fs.appendFileSync("./" + tempFile, line.toString().replaceAll(",", ".") + "\n");
+    }
+    rowCount++;
 });
 
 var input = fs.createReadStream(tempFile);
 
 
-parser.on("readable", function(){
-  var record;
-  while(record = parser.read()) {
-    txnLines++;
-    R.execute(record, function(result){
-      rdb.connect(dbconfig.rethinkdb, function(err, conn) {
-        if (err) {
-        console.log("Could not open a connection to initialize the database");
-        console.log(err.message);
-        process.exit(1);
-      }
-      rdb.table("txns").insert(result).run(conn);
-    });
-      saveTxn(result);
-    });
-  }
+parser.on("readable", function () {
+    var record;
+    while (record = parser.read()) {
+        txnLines++;
+        R.execute(record, function (result) {
+            rdb.connect(dbconfig.rethinkdb, function (err, conn) {
+                if (err) {
+                    console.log("Could not open a connection to initialize the database");
+                    console.log(err.message);
+                    process.exit(1);
+                }
+                rdb.table("txns").insert(result).run(conn);
+        });
+        saveTxn(result);
+        });
+    }
 });
 
-parser.on("error", function(error){
+parser.on("error", function (error) {
   console.log(" ** Error: " + error);
 });
 
-parser.on("end", function(){
+parser.on("end", function () {
   console.log("Parsing Completed, Total " + txnLines + " transactions processed!");
 });
 
